69166f1355198dcf12d1adf7f475552f731d4a1d
gprabha1 <ganesanprabhakar23@gmail.com>
2026-06-20 11:15:23 +0000

    collectlist

--- python/code.py
+++ python/code.py
@@ -1,1 +1,15 @@
 
+from pyspark.sql.functions import *
+
+column = ['Name','city','amount']
+values = [('zeyo','chennai',500),('sai','bangalore',1000),('zeyo','hyd',200),('sai','bangalore',1000),('zeyo','chennai',500),('sai','bangalore',5000),('zeyo','chennai',500),('sai','Hyd',1000)]
+
+df = spark.createDataFrame(values, column)
+
+finaldf = df.groupBy("name").agg(sum("amount").alias("total"), count("amount").alias("count"),collect_list("amount").alias("list"))
+#finaldf.show(10,False)
+
+df.createOrReplaceTempView("table")
+
+df = spark.sql("select name, sum(amount) as total, count(amount) as count, collect_list(amount) as list from table group by name").show(10,False)
+------------------------------------------------------------------------------------------------------------------------------
